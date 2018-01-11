d5ddc7433d191c0158195d0ca436b81e5ac2d535
SungMinCho <tjdals4565@gmail.com>
2018-01-11 10:15:06 +0000

    Appropriate file name

---
+++ main/sim_data/f1/code.py
@@ -0,0 +1,17 @@
+from main.models import *
+import random as rand
+
+sim = Simulation.objects.get(num=1)
+
+chart = Chart(sim=sim, name='random walk')
+chart.save()
+
+y = 0
+
+for x in range(1000):
+    if rand.random() < 0.5:
+        y += 1
+    else:
+        y -= 1
+    data = Ffdata(chart=chart, x=x, y=y)
+    data.save()
